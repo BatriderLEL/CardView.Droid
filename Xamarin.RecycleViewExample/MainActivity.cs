247ac80ce5be63baf2e07748993c0a75eb1d5edc
Vasyatka <robocop_1st@mail.ru>
2017-01-18 21:11:37 +0200

    * Xamarin.RecycleViewExample.sln: Fixed problem with commit
    * PhotoAlbums.cs:
    * MainActivity.cs:
    * packages.config:
    * RecyclerViewAdapter.cs:
    * AboutAssets.txt:
    * AssemblyInfo.cs:
    * Main.axml:
    * Styles.xml:
    * Icon.png:
    * top_menus.xml:
    * Strings.xml:
    * AboutResources.txt:
    * AndroidManifest.xml:
    * eurostar.jpg:
    * louvre_1.jpg:
    * louvre_2.jpg:
    * rue_cler.jpg:
    * big_ben_2.jpg:
    * big_ben_1.jpg:
    * Icon.png:
    * Icon.png:
    * royal_mile.jpg:
    * notre_dame.jpg:
    * london_eye.jpg:
    * Xamarin.RecycleViewExample.csproj:
    * Icon.png:
    * Icon.png:
    * seine_barge.jpg:
    * seine_river.jpg:
    * PhotoCardView.xml:
    * Icon.png:
    * to_notre_dame.jpg:
    * old_meets_new.jpg:
    * champ_elysees.jpg:
    * tower_visitors.jpg:
    * la_tour_eiffel.jpg:
    * scott_monument.jpg:
    * one_o_clock_gun.jpg:
    * pompidou_centre.jpg:
    * portcullis_gate.jpg:
    * tower_of_london.jpg:
    * victoria_albert.jpg:
    * arc_de_triomphe.jpg:
    * versailles_gates.jpg:
    * museum_and_castle.jpg:
    * inside_notre_dame.jpg:
    * edinburgh_station.jpg:
    * buckingham_guards.jpg:
    * heres_lookin_at_ya.jpg:
    * downtown_edinburgh.jpg:
    * edinburgh_castle_1.jpg:
    * medieval_siege_gun.jpg:
    * edinburgh_castle_2.jpg:
    * versailles_fountains.jpg:
    * before_mobile_phones.jpg:
    * modest_accomodations.jpg:
    * edinburgh_from_on_high.jpg:
    * view_from_holyrood_park.jpg:
    * ic_action_content_save.png:
    * ic_action_social_share.png:
    * ic_action_content_undo.png:
    * ic_action_social_share.png:
    * ic_action_content_save.png:
    * ic_action_content_redo.png:
    * ic_action_content_undo.png:
    * ic_action_content_redo.png:
    * ic_action_social_share.png:
    * ic_action_content_redo.png:
    * ic_action_content_undo.png:
    * ic_action_content_save.png:
    * ic_action_content_create.png:
    * ic_action_content_create.png:
    * ic_action_content_undo.png:
    * ic_action_social_share.png:
    * ic_action_content_save.png:
    * ic_action_content_redo.png:
    * ic_action_social_share.png:
    * ic_action_content_save.png:
    * ic_action_content_create.png:
    * ic_action_content_redo.png:
    * ic_action_content_undo.png:
    * ic_action_content_create.png:
    * ic_action_content_create.png:

---
+++ Xamarin.RecycleViewExample/MainActivity.cs
@@ -0,0 +1,85 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.Widget;
+using System;
+
+namespace Xamarin.RecycleViewExample
+{
+	[Activity(Label = "Xamarin.RecycleViewExample", MainLauncher = true, Icon = "@drawable/icon",Theme="@style/MyTheme")]
+	public class MainActivity : AppCompatActivity
+	{
+		RecyclerView RecView;
+		RecyclerView.LayoutManager RecManager;
+		RecyclerViewAdapter RecAdapter;
+		PhotoAlbums Albums;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			Toolbar ToolBar = FindViewById<Toolbar>(Resource.Id.EditToolBar);
+
+			SetSupportActionBar(ToolBar);
+			SupportActionBar.Title = "RecyclerView Demo";
+
+			RecView = FindViewById<RecyclerView>(Resource.Id.RecView);
+
+			RecManager = new LinearLayoutManager(this);
+			RecView.SetLayoutManager(RecManager);
+
+			Albums = new PhotoAlbums();
+
+			RecAdapter = new RecyclerViewAdapter(Albums);
+			RecAdapter.ItemClick += OnItemClick;
+
+			RecView.SetAdapter(RecAdapter);
+		}
+
+		public override bool OnCreateOptionsMenu(IMenu menu)
+		{
+			MenuInflater.Inflate(Resource.Menu.top_menus,menu);
+			return base.OnCreateOptionsMenu(menu);
+		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			switch (item.ItemId)
+			{
+				case Resource.Id.menu_edit:
+					RecManager = new GridLayoutManager(this,2,GridLayoutManager.Horizontal,false);
+					RecView.SetLayoutManager(RecManager);
+					return true;
+				case Resource.Id.menu_save:
+					if (Albums != null)
+					{
+						Albums.RandomSwap();
+						//RecAdapter.NotifyItemChanged(0);
+						//RecAdapter.NotifyItemChanged(Swapped);
+						RecAdapter.NotifyDataSetChanged();
+					}
+					return true;
+				default:
+					return base.OnOptionsItemSelected(item);
+			}
+			//Toast.MakeText(this, "Top ActionBar pressed: " + item.TitleFormatted, ToastLength.Short).Show();
+		}
+
+		void OnItemClick(object sender, int position)
+		{
+			int photoNum = position + 1;
+			Toast.MakeText(this, "This is photo number " + photoNum, ToastLength.Short).Show();
+		}
+
+
+	}
+
+
+}
+
---
+++ Xamarin.RecycleViewExample/RecyclerViewAdapter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace Xamarin.RecycleViewExample
+{
+	public class RecyclerViewAdapter : RecyclerView.Adapter
+	{
+		//underlying data(Osnovnaia data)
+		public PhotoAlbums PhotoAlbumsData;
+
+		public event EventHandler<int> ItemClick;
+
+		public RecyclerViewAdapter(PhotoAlbums PhotoAlbumsData) 
+		{
+			this.PhotoAlbumsData = PhotoAlbumsData;
+		}
+
+		public override int ItemCount
+		{
+			get
+			{
+				return PhotoAlbumsData.NumPhotos;
+			}
+		}
+
+		public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+		{
+			var ViewHolder = holder as PhotoAlbumViewHolder;
+
+			ViewHolder.Img.SetImageResource(PhotoAlbumsData[position].PhotoID);
+			ViewHolder.Txt.Text = PhotoAlbumsData[position].Caption;
+		}
+
+		public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+		{
+			var View = LayoutInflater.FromContext(parent.Context).Inflate(Resource.Layout.PhotoCardView, parent, false);
+
+			//PhotoAlbumViewHolder ViewHolder = new PhotoAlbumViewHolder(View);
+
+			return new PhotoAlbumViewHolder(View,OnClick);
+		}
+
+		void OnClick(int position)
+		{
+			if (ItemClick != null)
+				ItemClick(this, position);
+		}
+	}
+
+
+	class PhotoAlbumViewHolder : RecyclerView.ViewHolder
+	{
+		public TextView Txt { get; set; }
+		public ImageView Img { get; set; }
+
+		public PhotoAlbumViewHolder(View ItemView,Action<int> ItemClicked) : base(ItemView)
+		{
+			Txt = ItemView.FindViewById<TextView>(Resource.Id.Txt);
+			Img = ItemView.FindViewById<ImageView>(Resource.Id.Img);
+
+			ItemView.Click += (sender, e) => ItemClicked(base.Position);
+		}
+	}
+
+
+
+}
